35752a57f56e74678c1ff00647e2fd209f55abce
Dakin Deng <dengdajin@gmail.com>
2017-10-21 23:02:15 +0800

    Signed-off-by: Dakin Deng <dengdajin@gmail.com>

--- src/app/components/alert.component.ts
+++ src/app/components/alert.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
+declare var $: any;
 
 @Component({
-  selector: 'alert',
-  templateUrl: './alert.component.html' 
+    selector: 'x-alert',
+    templateUrl: './alert.component.html'
 })
 export class AlertComponent {
+    constructor(private el: ElementRef) {
+    }
+
     @Input()
     private shown: boolean = false;
+    $html: any;
+
+    ngAfterViewInit() {
+        //this.el.nativeElement
+        this.$html = $(this.el.nativeElement.childNodes[1]);
+    }
+
+    alert() {
+        window.document.body.appendChild(this.$html[0]); ///需要测试。
+
+        this.$html.alert()
+    }
+
+    close() {
+        this.$html.alert('close')
+    }
 }
